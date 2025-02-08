48ec876649aaaf753572d9793796277c29de0163
Chang Mao Yang <jeffrey0613mao@gmail.com>
2025-02-09 00:01:41 +0800

    show iframe

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 window.addEventListener('load', () => {
+	// for main page
+	addScrollEffectToSeeBackground();
+	clickToShowPoster();
+});
+
+function addScrollEffectToSeeBackground() {
 	const fullPage = document.querySelector('#full-page');
 	const observer = new IntersectionObserver((entries) => {
 			entries.forEach((entry) => {
@@ -14,4 +20,44 @@ window.addEventListener('load', () => {
 		}
 	);
 	observer.observe(fullPage);
-});
+}
+
+function removeCurrentPoster() {
+	const iframeContainer = document.querySelector('.iframe-container');
+	const iframe = iframeContainer.querySelector('iframe');
+	iframeContainer.style.display = "none";
+	iframeContainer.classList.remove("show");
+
+	// wait some time to remove the src
+	// setTimeout(()=>{
+	// 	if (iframeContainer.classList.contains("show")) return;
+	// 	iframe.src = "";
+	// }, 1000);
+}
+
+function clickToShowPoster() {
+	const iframeContainer = document.querySelector('.iframe-container');
+	const posters = document.querySelectorAll('.card[href^="#"]');
+	posters.forEach(poster => {
+		const clickEvent = ()=>{
+			const iframe = iframeContainer.querySelector('iframe');
+			if(!iframe) return;
+			iframe.src = poster.getAttribute('data-url');
+			setTimeout(()=>{
+				iframeContainer.style.display = "block";
+				iframeContainer.classList.add("show");
+			}, 0);
+		}
+		poster.addEventListener('click', clickEvent);
+	});
+	window.addEventListener('click', (e) => {
+		if (iframeContainer.classList.contains("show") 
+			&& !iframeContainer.contains(e.target) ) {
+			removeCurrentPoster();
+		}
+	});
+}
+
+
+
+
